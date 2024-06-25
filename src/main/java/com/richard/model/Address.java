4695a110fd4e091c8b4ea67c5117d6fd3438e33e
rwilliams <richyrichw93@gmail.com>
2024-06-25 17:06:48 +0200

    expand address fields

--- src/main/java/com/richard/model/Address.java
+++ src/main/java/com/richard/model/Address.java
@@ -4,10 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Entity
+@Data
+@EqualsAndHashCode(exclude = { "id" })
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String streetAddress;
+    private String zipCode;
+    private String city;
+    private String state;
+    private String country;
 }
