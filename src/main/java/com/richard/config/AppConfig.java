e0f043fac1a01e53d1487343523e026aa9fd7584
rwilliams <richyrichw93@gmail.com>
2024-09-02 15:40:14 +0200

    use servlet context path from application.properties

--- src/main/java/com/richard/config/AppConfig.java
+++ src/main/java/com/richard/config/AppConfig.java
@@ -23,27 +23,32 @@ import java.util.Collections;
 public class AppConfig {
     
     private final String jwtSecretKey;
+    private final String servletContextPath;
     
-    public AppConfig(@Value("${jwt.secret.key}") String jwtSecretKey) {
+    public AppConfig(
+            @Value("${jwt.secret.key}") String jwtSecretKey,
+            @Value("${server.servlet.context-path}") String servletContextPath) {
         this.jwtSecretKey = jwtSecretKey;
+        this.servletContextPath = servletContextPath;
     }
     
     /**
      * Configures the security filter chain for the application.
      *
-     * @param  http  the HttpSecurity object to configure
-     * @return       the SecurityFilterChain object
-     * @throws Exception  if an error occurs during configuration
+     * @param http the HttpSecurity object to configure
+     * @return the SecurityFilterChain object
+     * @throws Exception if an error occurs during configuration
      */
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         
         http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(authorize -> authorize
-                                                        .requestMatchers("/api/admin/**").hasAnyRole("RESTAURANT_OWNER", "ADMIN")
-                                                        .requestMatchers("/api/**").authenticated()
-                                                        .anyRequest().permitAll()
-                                  )
+            .authorizeHttpRequests(authorize -> authorize.requestMatchers(servletContextPath + "/admin/**")
+                                                         .hasAnyRole("RESTAURANT_OWNER", "ADMIN")
+                                                         .requestMatchers(servletContextPath + "/api/**")
+                                                         .authenticated()
+                                                         .anyRequest()
+                                                         .permitAll())
             .addFilterBefore(new JwtTokenValidator(jwtSecretKey), BasicAuthenticationFilter.class)
             .csrf(AbstractHttpConfigurer::disable)
             .cors(cors -> cors.configurationSource(corsConfigurationSource()));
@@ -52,18 +57,15 @@ public class AppConfig {
     }
     
     /**
-     * Returns a CorsConfigurationSource object that specifies the allowed origins, methods, headers, credentials, and max age for CORS
-     * requests.
+     * Returns a CorsConfigurationSource object that specifies the allowed origins, methods, headers, credentials, and max age for
+     * CORS requests.
      *
      * @return a CorsConfigurationSource object with the specified configuration
      */
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000",
-                "http://localhost:8080"
-                                                     ));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.setExposedHeaders(Collections.singletonList("Authorization"));
