51764b6e0772122681d7f8b494677579e9332e76
r.williams <richard.williams@logius.nl>
2025-01-05 00:13:23 +0100

    add api/v1

--- src/main/java/com/richard/controller/order/OrderController.java
+++ src/main/java/com/richard/controller/order/OrderController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/order")
+@RequestMapping("api/v1/order")
 public class OrderController {
     
     private final OrderService orderService;
